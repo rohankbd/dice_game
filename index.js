da4ce7f28145b291763ade64aa96e74e815b9c16
rohankbd <86970070+rohankbd@users.noreply.github.com>
2022-11-12 23:33:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber1)
+
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randomNumber2)
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩👈 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 👉🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "👉Draw!👈";
+}
